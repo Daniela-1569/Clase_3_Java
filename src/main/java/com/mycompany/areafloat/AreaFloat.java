92c34a0671fa804a2e38e7e296b80ef770805eff
Daniela-1569 <danielavalladolid04@gmail.com>
2025-04-02 11:01:41 -0500

    termine el primer programa

---
+++ src/main/java/com/mycompany/areafloat/AreaFloat.java
@@ -0,0 +1,68 @@
+
+package com.mycompany.areafloat;
+
+/**
+ *
+ * @author PC01
+ */
+public class AreaFloat {
+
+    public static void main(String[] args) {
+  
+  /* double base=2;
+   double altura=4;
+   double area=0;*/
+   
+   area=base*altura;
+   
+        System.out.println("la altura del resvtangulo es:"+area); 
+   
+  //entreda
+  int num=2; 
+        if (num> 0) {
+            System.out.println("el numero es positivo:"+num);
+            {
+              else
+            }
+             Entrada
+        int num=2;
+        if (num>0){
+            System.out.println("El numero ingresado es positivo:"+num);
+        }
+        else System.out.println("Error, el numero es negativo");
+            
+        }
+   /*ejercicio 3
+   escribe un programa que determine si un  numero es par o impar. */
+   
+   int num=5;
+   int operacion=num%2;
+        if (operacion==0) {
+            System.out.println("el numero es par:");
+        }
+        else{
+            System.out.println("el numero es impar"); */ 
+        }
+  // ejercicio 4 
+  escribe un programa que determine si un estudiante aprueba o reprueba 
+          segun su nota.
+          
+         int nota=80;
+         
+        if ((nota>=0)&&(nota<=100)){
+        if (nota>=90) {
+            System.out.println("Exelente");
+        }else if ((nota>=70)&&(nota<=89)){  
+            System.out.println("Aprobado");   
+        }else if (nota<70){
+            System.out.println("Reprobado");
+        }else {
+            System.out.println("Su nota es inavalida");
+        }
+        
+    }
+        
+ 
+    } 
+        
+        
